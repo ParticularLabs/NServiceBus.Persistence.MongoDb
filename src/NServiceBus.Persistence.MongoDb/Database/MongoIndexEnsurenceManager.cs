5a6ce452479ef59986caea2b44a81504485ee3d2
k-tomak <tomak.ksu@gmail.com>
2017-03-30 21:18:57 +0300

    Rename refactorings

--- src/NServiceBus.Persistence.MongoDb/Database/MongoIndexEnsurenceManager.cs
+++ src/NServiceBus.Persistence.MongoDb/Database/MongoIndexEnsurenceManager.cs
@@ -12,7 +12,7 @@ namespace NServiceBus.Persistence.MongoDB.Database
         private const string IndexName = "name";
         private const string BsonDocumentKey = "key";
 
-        private enum IndexExistence
+        private enum IndexEnsurenceAction
         {
             Create,
             Recreate,
@@ -23,12 +23,12 @@ namespace NServiceBus.Persistence.MongoDB.Database
             IMongoIndexManager<T> indexManager, BsonDocument key, CreateIndexOptions options,
             IEnumerable<BsonDocument> indexes);
 
-        private static readonly Dictionary<IndexExistence, IndexExistenceDelegate> IndexExistenceHandlers = new Dictionary
-            <IndexExistence, IndexExistenceDelegate>()
+        private static readonly Dictionary<IndexEnsurenceAction, IndexExistenceDelegate> IndexEnsurenceActionHandlers = new Dictionary
+            <IndexEnsurenceAction, IndexExistenceDelegate>()
         {
-            {IndexExistence.Create, OnNoneIndex},
-            {IndexExistence.Recreate, OnDifferentOprionsIndex},
-            {IndexExistence.None, (manager, index, options, indexes) => Task.FromResult(false)}
+            {IndexEnsurenceAction.Create, CreateIndex},
+            {IndexEnsurenceAction.Recreate, DropAndCreateIndex},
+            {IndexEnsurenceAction.None, (manager, index, options, indexes) => Task.FromResult(false)}
         };
 
 
@@ -39,11 +39,11 @@ namespace NServiceBus.Persistence.MongoDB.Database
             if (options == null)
                 options = new CreateIndexOptions();
             var collectionIndexes = await indexManager.IndexesToList().ConfigureAwait(false);
-            var existence = IndexExists(collectionIndexes, indexToEnsure, options);
-            await IndexExistenceHandlers[existence].Invoke(indexManager, indexToEnsure, options, collectionIndexes).ConfigureAwait(false);
+            var action = IndexExists(collectionIndexes, indexToEnsure, options);
+            await IndexEnsurenceActionHandlers[action].Invoke(indexManager, indexToEnsure, options, collectionIndexes).ConfigureAwait(false);
         }
 
-        private static IndexExistence IndexExists(IEnumerable<BsonDocument> collectionIndexes, BsonDocument indexToEnsure,
+        private static IndexEnsurenceAction IndexExists(IEnumerable<BsonDocument> collectionIndexes, BsonDocument indexToEnsure,
             CreateIndexOptions options)
         {
             var existingIndex = collectionIndexes.FirstOrDefault(index =>
@@ -51,7 +51,7 @@ namespace NServiceBus.Persistence.MongoDB.Database
 
             //no such index key -> create index
             if (existingIndex == null)
-                return IndexExistence.Create;
+                return IndexEnsurenceAction.Create;
 
             //if nonunique index required, no matter unique/nonunique index was created before -> do nothing
             //if unique index required and current index options are the same -> do nothing
@@ -61,38 +61,38 @@ namespace NServiceBus.Persistence.MongoDB.Database
                         index[BsonDocumentKey].Equals(indexToEnsure) &&
                         index.Contains(UniquenessKey)
                         && index[UniquenessKey].AsBoolean == true))
-                return IndexExistence.None;
+                return IndexEnsurenceAction.None;
 
             //collection has index on same fields with different options with name by default -> drop and create index
-            return IndexExistence.Recreate;
+            return IndexEnsurenceAction.Recreate;
         }
 
 
         #region Existence handlers
 
         /// <summary>
-        /// Handles Create index. Creates index.
+        /// Creates index.
         /// </summary>
         /// <param name="indexManager">Mongo index manager.</param>
         /// <param name="indexToEnsure">Index to recreate.</param>
         /// <param name="options">Options for creating an index.</param>
         /// <param name="collectionIndexes">Set of collection indexes.</param>
         /// <returns>Task to await index recreation.</returns>
-        private static Task OnNoneIndex(IMongoIndexManager<T> indexManager, BsonDocument indexToEnsure,
+        private static Task CreateIndex(IMongoIndexManager<T> indexManager, BsonDocument indexToEnsure,
             CreateIndexOptions options, IEnumerable<BsonDocument> collectionIndexes)
         {
             return indexManager.CreateOneAsync(new BsonDocumentIndexKeysDefinition<T>(indexToEnsure), options);
         }
 
         /// <summary>
-        /// Handles DifferentOprionsIndex. Drops and recreates index because it was created with different options.
+        /// Drops and recreates index because it was created with different options.
         /// </summary>
         /// <param name="indexManager">Mongo index manager.</param>
         /// <param name="indexToEnsure">Index to recreate.</param>
         /// <param name="options">Options for creating an index.</param>
         /// <param name="collectionIndexes">Set of collection indexes.</param>
         /// <returns>Task to await index recreation.</returns>
-        private static async Task OnDifferentOprionsIndex(IMongoIndexManager<T> indexManager, BsonDocument indexToEnsure,
+        private static async Task DropAndCreateIndex(IMongoIndexManager<T> indexManager, BsonDocument indexToEnsure,
             CreateIndexOptions options, IEnumerable<BsonDocument> collectionIndexes)
         {
             // index can have custom name
